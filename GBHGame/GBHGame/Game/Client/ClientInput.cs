7b4e45f9b358a9c082da8d3aeeca21dcca8acef6
avail <avail@pomf.se>
2016-08-21 01:43:26 +0200

    basic controller support

--- GBHGame/GBHGame/Game/Client/ClientInput.cs
+++ GBHGame/GBHGame/Game/Client/ClientInput.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
 
 namespace GBH
 {
@@ -18,41 +19,54 @@ namespace GBH
         private static bool _upKey;
         private static bool _downKey;
         private static bool _shootKey;
+        private static float _vibrationAmount = 0.4f;
 
         public static void CreateCommand()
         {
             var command = new UserCommand();
             command.ServerTime = (uint)(Game.Time + Client.TimeBase);
 
-            if (_upKey)
+            GamePadState gamePad = GamePad.GetState(PlayerIndex.One);
+
+            // todo: deadzones?
+            if (_upKey || gamePad.ThumbSticks.Left.Y >= 0.05f)
             {
                 command.Buttons |= ClientButtons.Forward;
             }
 
-            if (_downKey)
+            if (_downKey || gamePad.ThumbSticks.Left.Y <= -0.05f)
             {
                 command.Buttons |= ClientButtons.Backward;
             }
 
-            if (_leftKey)
+            if (_leftKey || gamePad.ThumbSticks.Right.X <= -0.05f)
             {
                 command.Buttons |= ClientButtons.RotateLeft;
             }
 
-            if (_rightKey)
+            if (_rightKey || gamePad.ThumbSticks.Right.X >= 0.05f)
             {
                 command.Buttons |= ClientButtons.RotateRight;
             }
 
-            if (_shootKey)
+            if (_shootKey || gamePad.Buttons.RightShoulder == ButtonState.Pressed)
             {
                 command.Buttons |= ClientButtons.Shoot;
+                // vibrate controller when shooty
+                if (GamePad.GetState(PlayerIndex.One).IsConnected)
+                {
+                    _vibrationAmount = MathHelper.Clamp(_vibrationAmount + 0.03f, 0.0f, 1.0f);
+                    GamePad.SetVibration(PlayerIndex.One, _vibrationAmount, _vibrationAmount);
+                }
             }
 
             _userCommands.Enqueue(command);
 
             _commandNum++;
             _userCommandBackups[_commandNum & (UCMD_BACKUP - 1)] = command;
+
+            // stop vibration
+            GamePad.SetVibration(PlayerIndex.One, 0.0f, 0.0f);
         }
 
         public static int CommandID
